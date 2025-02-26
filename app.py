597c72fbc7488f19054fbfcda8b9c195a7e32b9d
Nickdiaz14 <elkindiaz1931@gmail.com>
2025-02-25 22:01:28 -0500

    Added picas y fijas game

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/games', methods=['POST'])
 def games():
     game = str(request.json['game'])
-    num = float(request.json['num'])
+    num = request.json['num']
     if game == '1':
         valid, aproach = tendencias(game,num)
     return jsonify({'flag': valid, 'aproach': aproach})
@@ -49,10 +49,68 @@ def input_page():
 @app.route('/leaderboard')
 def leader_page():
     game = request.args.get('game')
-    return render_template('leaderboard.html',data=json.dumps(get_top_scores(game)))
+    if game == '1':
+        return render_template('leaderboard.html',data=json.dumps(get_top_scores(game)))
+    else:
+        return render_template('leaderboard.html',data=json.dumps(get_top_scores_picas(game)))
 
 #----------------------------------------------------------------------------------------------------------------------------------------
+def p_y_f(game,num):
+    valid = True
+    aproach = 0
+    connection = connect_db()
+    cursor = connection.cursor()
+
+    
+    cursor.execute("""
+        SELECT number FROM public.tendencias
+        WHERE aproach = 0 AND board = %s""",(game,))
+    
+    flag = cursor.fetchone()
+
+    if not flag:
+        valid = False
+        cursor.execute("""
+            SELECT number FROM public.tendencias
+            WHERE aproach = -1 AND board = %s""",(game,))
+        
+        oficial_num = cursor.fetchone()
+
+        if not oficial_num:
+            cursor.execute("""
+                INSERT INTO tendencias (number, aproach, board)
+                VALUES (%s, %s, %s);""",(random.randint(1000,9999),-1,game))
+            
+            cursor.execute("""
+                SELECT number FROM public.tendencias
+                WHERE aproach = -1 AND board = %s""",(game,))
+            
+            oficial_num = cursor.fetchone()
+
+        oficial_num = oficial_num[0]
+        for i in range(4):
+            aproach+=abs(int(oficial_num[i]) - int(num[i]))
+
+        count_spades,count_fixed=0,0
+        array_fixed=[]
+        for i in range(4):
+            if num[i]==num[i]:
+                count_fixed+=1
+                array_fixed.append(num[i])
+            elif (num[i] in num) & (num[i:].count(num[i])==1) & (num[i] not in array_fixed):
+                count_spades+=1
+
+        cursor.execute("""
+            INSERT INTO tendencias (number, aproach, percentage, color, board)
+            VALUES (%s, %s, %s, %s, %s);""",(int(num), aproach, count_spades, count_fixed,game))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return valid, aproach
+
 def tendencias(game,num):
+    num = float(num)
     valid = True
     aproach = -1
     connection = connect_db()
@@ -83,7 +141,6 @@ def tendencias(game,num):
                 WHERE aproach = -1 AND board = %s""",(game,))
             
             oficial_num = cursor.fetchone()
-            print(oficial_num)
 
         oficial_num = int(oficial_num[0])
         aproach = (num)/oficial_num
@@ -129,6 +186,25 @@ def get_top_scores(game):
             LIMIT 10
         ) AS subquery
         ORDER BY subquery.aproach ASC;
+    """,(game,))
+
+    results = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    
+    return results
+
+def get_top_scores_picas(game):
+    connection = connect_db()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT number, percentage, color
+        FROM public.tendencias
+        WHERE aproach != -1 AND board = %s
+        ORDER BY aproach ASC
+        LIMIT 10;
     """,(game,))
 
     results = cursor.fetchall()
